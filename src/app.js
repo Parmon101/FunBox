3d57117936dd0cda676a9b5d0d10c23ee8d869bf
Evgeny Belyakov <bfjeka101@gmail.com>
2022-06-19 21:24:11 +0300

    add webpack

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.scss';
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>FunBox</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from './app';
+
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
